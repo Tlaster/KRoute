7e37084212806826fbf5adb73c6f9f285f71234e
Tlaster <tlaster@outlook.com>
2022-01-25 18:51:44 +0800

    add jitpack

--- kroute/build.gradle.kts
+++ kroute/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
 
 repositories {
@@ -11,4 +12,19 @@ dependencies {
     implementation("com.google.devtools.ksp:symbol-processing-api:1.6.10-1.0.2")
     implementation("com.squareup:kotlinpoet:1.10.2")
     implementation("com.squareup:kotlinpoet-ksp:1.10.2")
+}
+
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "moe.tlaster"
+                artifactId = "kroute"
+                version = "1.0.0"
+
+                from(components["java"])
+            }
+        }
+    }
 }
